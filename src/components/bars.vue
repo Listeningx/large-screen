f315a7d5570719ffa645a7249efe4697a3663aa7
listening <292973134@qq.com>
2023-04-11 10:34:04 +0800

    4.11

---
+++ src/components/bars.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <VueDragResize :isActive="true"  :isResizable="true"  :z="999">
+        <img src="@/assets/img/bg-1.png" v-drag height="100px" width="100px" />
+      </VueDragResize>
+    </div>
+  </template>
+  
+  <style>
+  .vdr.active:before{
+    outline: none
+  }
+  </style>
+  
+  <script>
+  import VueDragResize from 'vue-drag-resize'
+  export default {
+    name: 'littleBar',
+    components: {
+      VueDragResize
+    },
+    data () {
+      return {}
+    },
+    methods: {},
+    mounted () {
+  
+    }
+  }
+  </script>
+  
