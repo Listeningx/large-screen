e051c1c8be4443e2cf9d25e37787472184235c5b
listening <292973134@qq.com>
2023-05-16 15:07:45 +0800

    5.16

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <a href="http://39.105.194.84:8002/webroot/decision" target="_blank">打开中国电网展示大屏</a>
+        <img src="../assets/img/wind.png" width="100" height="100" />
         <!-- <iframe id="reportFrame" width="900" height="600" src="http://39.105.194.84:8002/webroot/decision"></iframe> -->
     </div>
 </template>
